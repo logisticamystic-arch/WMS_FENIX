867c2c8a028943a2f1b52d3ed22678d59a5c32e4
camilopc2012 <camilopc2012@gmail.com>
2026-07-23 17:40:01 -0500

    auto-sync: 2026-07-23 17:40:01

--- public/diag_schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable('.')->load();
-$pdo = new PDO('pgsql:host='.$_ENV['DB_HOST'].';port=5432;dbname='.$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
-$stmt = $pdo->query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'picking_detalles'");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
